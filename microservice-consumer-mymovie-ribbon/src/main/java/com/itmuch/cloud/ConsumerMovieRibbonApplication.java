05e0f15423b224b7a710d1b6dce74186f871f1df
xfpan <1318448581@qq.com>
2018-01-18 17:29:54 +0800

    ribbon修改2

--- microservice-consumer-mymovie-ribbon/src/main/java/com/itmuch/cloud/ConsumerMovieRibbonApplication.java
+++ microservice-consumer-mymovie-ribbon/src/main/java/com/itmuch/cloud/ConsumerMovieRibbonApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import com.itmuch.config.TestConfiguration;
+
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "microservice-provider-myuser", configuration = TestConfiguration.class)
 public class ConsumerMovieRibbonApplication {
 
   @Bean
