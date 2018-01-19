36c996edb1718ab9cf2da9a9f385640440f7884e
xfpan <1318448581@qq.com>
2018-01-19 13:38:13 +0800

    ribbon-properties

---
+++ microservice-consumer-mymovie-ribbon-properties-customizing/src/main/java/com/itmuch/cloud/controller/MovieController.java
@@ -0,0 +1,31 @@
+package com.itmuch.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.itmuch.cloud.entity.User;
+
+@RestController
+public class MovieController {
+  @Autowired
+  private RestTemplate restTemplate;
+  
+  @Autowired
+  private LoadBalancerClient loadBalancerClient;
+
+  @Value("${user.userServicePath}")
+  private String userServicePath;
+
+  @GetMapping("/movie/{id}")
+  public User findById(@PathVariable Long id) {
+	  ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-myuser");
+	  System.out.println("application=="+serviceInstance.getServiceId()+":"+serviceInstance.getHost()+":"+serviceInstance.getPort());
+    return this.restTemplate.getForObject(this.userServicePath + id, User.class);
+  }
+}
